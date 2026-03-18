39da88a5eb145730872b65b171bfb601e10ef24c
Melonisjy <79883658+Melonisjy@users.noreply.github.com>
2026-03-18 12:57:42 +0900

    feat: import globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import Providers from './providers';
 
+import './globals.css';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
