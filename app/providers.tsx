775e693673d175171fece0e4e3022201a9a52896
Melonisjy <dhy2457@naver.com>
2026-03-22 12:10:10 +0900

    fix: set up the detaultChain to baseSepolia

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { PrivyProvider } from '@privy-io/react-auth';
+import { baseSepolia } from 'viem/chains';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
@@ -8,6 +9,8 @@ export default function Providers({ children }: { children: React.ReactNode }) {
             appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
             config={{
                 loginMethods: ['google', 'email'],
+                supportedChains: [baseSepolia],
+                defaultChain: baseSepolia,
                 embeddedWallets: {
                     ethereum: {
                         createOnLogin: 'users-without-wallets',
